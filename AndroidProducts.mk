fa7c6ebb7c1fe2cb684d6384fdd372e44d7c960a
Ar5646L <aryan.v.bhatt@gmail.com>
2023-08-20 02:50:06 +0530

    munch: Pixel Experience Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_munch.mk
+    $(LOCAL_DIR)/aosp_munch.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_munch-user \
-    arrow_munch-userdebug \
-    arrow_munch-eng
+    aosp_munch-user \
+    aosp_munch-userdebug \
+    aosp_munch-eng
